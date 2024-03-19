ba1b5ff6a18eea3cab5f8aa22bec5df3d791f235
Ronal Casani <ronalsistemas@gmail.com>
2024-03-18 22:49:36 -0500

    Fin de sección 4

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+
+  public name: string = 'iroman';
+  public age: number = 45;
+
+  get capitalizeName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${ this.name } - ${ this.age }`;
+  }
+
+  chageHero(): void {
+    this.name = 'spiderman';
+  }
+
+  changeAge(): void {
+    this.age = 25;
+  }
+
+  resetForm(): void {
+    this.name = 'ironman';
+    this.age = 45;
+  }
+}
